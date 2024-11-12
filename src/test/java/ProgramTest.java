195d5f540ae8020eb63c07da4bde6a5415010538
chinvlinh <khongbachofficial@gmail.com>
2024-11-12 22:16:16 +0700

    push

--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -9,17 +9,14 @@ public class ProgramTest {
 
     private Program program;
 
+    // Trước mỗi bài kiểm tra, tạo đối tượng Program mới
     @Before
     public void setUp() {
-        // program = new Program();
+        program = new Program();
     }
 
     @Test
     public void testLabels() {
-        // assertEquals("Lê Đình Chinh", program.getL1().getText());
-        // assertEquals("Lê Công Huy", program.getL2().getText());
-        // assertEquals("Nguyễn Duy Vũ", program.getL3().getText());
-        // assertEquals("Nguyễn Nhật Linh", program.getL4().getText());
-        // assertEquals("Nhóm 1", program.getNameGroup().getText());
+        assertEquals("Group 1", program.getNameGroup().getText());
     }
 }
